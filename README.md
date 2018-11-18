b92162bbc0350c28df6debdc4cda482519a8e5ec
Phil Wilko <philwilkinson40@gmail.com>
2018-11-18 14:46:29 +0800

    _

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # micropython_BH1750
-theis
+super simple example use of BH1750 ambient light sensor on the [esp8266 port of micropython](https://micropython.org/download#esp8266)
+  i used the [lolin esp8266  D1 mini pro board](https://wiki.wemos.cc/products:d1:d1_mini_pro) and the [BH1750](https://wiki.wemos.cc/products:d1_mini_shields:ambient_light_shield)
+![](shield.jpg)   
+  
+BH1750 library module is from (https://github.com/PinkInk/upylib/tree/master/bh1750) I think
--- main.py
+++ main.py
@@ -2,11 +2,25 @@
 import bh1750
 import machine
 
-scl = machine.Pin(5)
-sda = machine.Pin(4)
-i2c = machine.I2C(scl,sda)
+i2c = machine.I2C(scl = machine.Pin(5), sda = machine.Pin(4))
+
+'''
+confirm your device is present on the bus
+it should return [35] for this particular sensor
+if this returns nothing, check your wiring
+then check the Pin GPIO numbers are correct 
+'''
+
+print(i2c.scan())
 
 s = bh1750.BH1750(i2c)
 
 while True:
     s.luminance(BH1750.ONCE_HIRES_1)
+
+'''
+there are lots of options to sense luminance
+read bh1750.py for details
+this is a high resolution single reading
+it returns a float of luminance in LUX
+'''
